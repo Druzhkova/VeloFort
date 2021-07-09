2960b4443ec17fa45fc936c8e0edc1a0b795d21e
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-09 18:40:16 +0300

    refactor: apply hover svg

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,24 +1,5 @@
-import bike from './bike.svg';
-import dashboard from './dashboard.svg';
-import localLibrary from './local-library.svg';
-import settings from './settings.svg';
-
-import bikeHover from './bike-hover.svg';
-import dashboardHover from './dashboard-hover.svg';
-import localLibraryHover from './local-library-hover.svg';
-import settingsHover from './settings-hover.svg';
-
 import btnPlay from './btn-play.svg';
 
-
 export {
-    bike,
-    dashboard,
-    localLibrary,
-    settings,
-    bikeHover,
-    dashboardHover,
-    localLibraryHover,
-    settingsHover,
     btnPlay
 }
