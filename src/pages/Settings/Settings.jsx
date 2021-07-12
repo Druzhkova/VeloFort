e61250ab0a1a0eca5fd61cd10fb3b306ee901087
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-12 16:43:54 +0300

    feat: makeup settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Dropdown } from '../../components';
+import styles from './Settings.module.css';
 
 export const Settings = () => {
   const { t } = useTranslation();
@@ -8,6 +9,17 @@ export const Settings = () => {
     <>
       <h1>{t('settings')}</h1>
       <Dropdown />
+      <div className={styles.inner}>
+        <h2 className={styles.title}>Оффлайн Аудиогид</h2>
+        <p>Позволяет загрузить необходимые файлы для прослушивания без подключения к интернету</p>
+        <div className={styles.button}>Загрузить</div>
+      </div>
+      <div className={styles.inner}>
+        <h2 className={styles.title}>Скачать трек</h2>
+        <p>Маршрут содержит контрольные точки, полезную информацию об исторических объектах и всего, что вам понадобиться в пути</p>
+        <div className={styles.button}>Скачать трек</div>
+        <div className={styles.buttonWhite}>Скачать трек</div>
+      </div>
     </>
   );
 };
