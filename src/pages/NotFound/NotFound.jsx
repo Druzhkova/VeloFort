6a2e5abba9ad275da914b2e0f7f5092a371abab3
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-13 14:40:08 +0300

    feat: add not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,26 @@
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import { notFound } from '../../assets';
+import styles from './NotFound.module.css';
 
 export const NotFound = () => {
   const { t } = useTranslation();
+  const history = useHistory();
 
   return (
-    <>
-      <h1>{t('notFoundPage.not_found')}</h1>
-    </>
+    <div className={styles.inner}>
+      <div className={styles.buttonContainer} onClick={() => history.push('/')}>
+        <button className={styles.button}>
+          &#129044;
+          {' '}
+          {t('notFoundPage.btn_home_page')}
+        </button>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.title}>{t('notFoundPage.title')}</p>
+        <span>{t('notFoundPage.subtitle')}</span>
+      </div>
+      <img className={styles.img} src={notFound} alt="" />
+    </div>
   );
 };
