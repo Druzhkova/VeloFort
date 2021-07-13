a5c3b7ca0f61191045d2fd9584d482c78025e717
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-13 09:47:13 +0300

    refactor: move translations

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import App from './App';
-import { resources } from './locales';
+import { resources } from './assets/locales';
 
 i18n
   .use(initReactI18next)
