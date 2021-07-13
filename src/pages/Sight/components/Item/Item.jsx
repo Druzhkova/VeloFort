30853500ce5b00a9abb35cc8cd2c2ea237155f77
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-13 13:17:17 +0300

    feat:

--- src/pages/Sight/components/Item/Item.jsx
+++ src/pages/Sight/components/Item/Item.jsx
@@ -1,33 +1,38 @@
+import { useTranslation } from 'react-i18next';
 import styles from './Item.module.css';
 import { marker, timeline, watch } from '../../../../assets';
 
 export const Item = ({
   img, title, time, distance,
-}) => (
-  <div className={styles.wrapper}>
-    <span className={styles.imgContainer}>
-      <img className={styles.img} src={img} alt={title} />
-    </span>
-    <div className={styles.content}>
-      <div className={styles.inner}>
-        <img className={styles.marker} src={marker} alt="Marker" />
-        <span className={styles.title}>{title}</span>
-      </div>
-      <div className={styles.inner}>
-        <img className={styles.timeLine} src={timeline} alt="Timeline" />
-        <span className={styles.distance}>
-          {distance}
-          км.
-        </span>
-      </div>
-      <div className={styles.inner}>
-        <img className={styles.watch} src={watch} alt="Watch" />
-        <span className={styles.time}>
-          {time}
-          ч.
-        </span>
-      </div>
+}) => {
+  const { t } = useTranslation();
 
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.imgContainer}>
+        <img className={styles.img} src={img} alt={title} />
+      </span>
+      <div className={styles.content}>
+        <div className={styles.inner}>
+          <img className={styles.marker} src={marker} alt="Marker" />
+          <span className={styles.title}>{t(`sightObject.${title}.title`)}</span>
+        </div>
+        <div className={styles.inner}>
+          <img className={styles.timeLine} src={timeline} alt="Timeline" />
+          <span className={styles.distance}>
+            {distance}
+            км.
+          </span>
+        </div>
+        <div className={styles.inner}>
+          <img className={styles.watch} src={watch} alt="Watch" />
+          <span className={styles.time}>
+            {time}
+            ч.
+          </span>
+        </div>
+
+      </div>
     </div>
-  </div>
-);
+  );
+};
