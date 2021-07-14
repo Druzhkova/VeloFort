5f8cb1e77e7c33515da9adfe525a8b0cfcfbff73
Viktoriya Druzhkova <viktoriya.druzhkova@solbeg.com>
2021-07-14 12:05:21 +0300

    feat: translation of information page

--- src/pages/Information/Information.jsx
+++ src/pages/Information/Information.jsx
@@ -10,12 +10,12 @@ export const Information = () => {
 
   return (
     <>
-      <h1>{t('informationPage.information')}</h1>
+      <h1>{t('informationPage.title')}</h1>
       <div className={styles.news}>
         <div className={styles.inner}>
           <div className={styles.content}>
             <img src={schedule} alt="Schedule" />
-            <h2 className={styles.title}>Новости</h2>
+            <h2 className={styles.title}>{t('informationPage.news')}</h2>
           </div>
           <div className={styles.arrow}>
             <img src={arrow} alt="Arrow" />
@@ -26,13 +26,13 @@ export const Information = () => {
         <div className={styles.lodging}>
           <div className={styles.content}>
             <img src={lodging} alt="Lodging" />
-            <h2 className={styles.title}>Проживание</h2>
+            <h2 className={styles.title}>{t('informationPage.lodging')}</h2>
           </div>
         </div>
         <div className={styles.food}>
           <div className={styles.content}>
             <img src={food} alt="Food" />
-            <h2 className={styles.title}>Питание</h2>
+            <h2 className={styles.title}>{t('informationPage.food')}</h2>
           </div>
         </div>
       </div>
@@ -40,7 +40,7 @@ export const Information = () => {
         <div className={styles.inner}>
           <div className={styles.content}>
             <img src={cyclist} alt="Pharmacy" />
-            <h2 className={styles.title}>Аптеки для велосипедиста</h2>
+            <h2 className={styles.title}>{t('informationPage.pharmacy')}</h2>
           </div>
           <div className={styles.arrow}>
             <img src={arrow} alt="Arrow" />
@@ -49,7 +49,7 @@ export const Information = () => {
       </div>
       <div className={styles.path}>
         <div className={styles.pathInner}>
-          <h2 className={styles.title}>Как пройти границу</h2>
+          <h2 className={styles.title}>{t('informationPage.path')}</h2>
           <div className={styles.arrow}>
             <img src={arrow} alt="Arrow" />
           </div>
@@ -59,7 +59,7 @@ export const Information = () => {
         <div className={styles.projectInner}>
           <div className={styles.content}>
             <img src={project} alt="About the project" />
-            <h2 className={styles.title}>О проекте</h2>
+            <h2 className={styles.title}>{t('informationPage.about')}</h2>
           </div>
           <div className={styles.arrow}>
             <img src={arrow} alt="Arrow" />
@@ -68,9 +68,9 @@ export const Information = () => {
       </div>
       <div className={styles.support}>
         <div className={styles.supportInner}>
-          <h2 className={styles.title}>Поддержать проект</h2>
-          <p>Даже +1 рубль говорит нам, что наша работа нужна и важна. Все это мы стараемся сделать бесплатным и доступным для вас!</p>
-          <Button>Поддержать</Button>
+          <h2 className={styles.title}>{t('informationPage.support_the_project')}</h2>
+          <p>{t('informationPage.support_the_project_title')}</p>
+          <Button>{t('informationPage.suppor')}</Button>
         </div>
       </div>
     </>
